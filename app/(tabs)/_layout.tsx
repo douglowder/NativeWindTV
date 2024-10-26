1266e4d303b0026fae81215a45e9dc6acd9afd2c
Douglas Lowder <douglowder@mac.com>
2024-10-26 03:22:26 -0700

    Rework tabs navigator for react-native-bottom-tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Link } from 'expo-router';
 import { Platform, Pressable } from 'react-native';
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import { withLayoutContext } from 'expo-router';
+import { createNativeBottomTabNavigator } from 'react-native-bottom-tabs/react-navigation';
+
+export const Tabs = withLayoutContext(
+  createNativeBottomTabNavigator().Navigator,
+);
 
 const scale = Platform.OS === 'ios' ? 2 : 1;
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-/*
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return (
-    <FontAwesome size={30 * scale} style={{ marginBottom: -3 }} {...props} />
-  );
-}
- */
-
-const tabBarButton = (props: BottomTabBarButtonProps) => {
-  const style: any = props.style ?? {};
-  return (
-    <Pressable
-      {...props}
-      style={({ pressed, focused }) => [
-        style,
-        {
-          opacity: pressed || focused ? 0.6 : 1.0,
-          color: focused ? 'green' : undefined,
-        },
-      ]}
-    />
-  );
-};
 
 export default function TabLayout() {
   return (
@@ -67,7 +42,6 @@ export default function TabLayout() {
           width: 50 * scale,
           height: 50 * scale,
         },
-        tabBarButton,
         tabBarStyle: {
           height: 100 * scale,
         },
@@ -89,14 +63,12 @@ export default function TabLayout() {
         name="two"
         options={{
           title: 'Two',
-          // tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <Tabs.Screen
         name="tvdemo"
         options={{
           title: 'TV Demo',
-          // tabBarIcon: ({ color }) => <TabBarIcon name="tv" color={color} />,
         }}
       />
     </Tabs>
