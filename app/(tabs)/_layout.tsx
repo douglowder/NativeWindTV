a1b2dfac7fecd26dd7d8ff5c964f5934962e240f
Douglas Lowder <douglowder@mac.com>
2024-10-28 11:57:36 -0700

    Fix tab layout, add icons for Apple TV

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,74 +1,37 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link } from 'expo-router';
-import { Platform, Pressable } from 'react-native';
+import { Platform } from 'react-native';
 import { withLayoutContext } from 'expo-router';
 import { createNativeBottomTabNavigator } from 'react-native-bottom-tabs/react-navigation';
 
-export const Tabs = withLayoutContext(
-  createNativeBottomTabNavigator().Navigator,
-);
+const { Navigator } = createNativeBottomTabNavigator();
 
-const scale = Platform.OS === 'ios' ? 2 : 1;
+export const Tabs = withLayoutContext<any, typeof Navigator, any, any>(
+  Navigator,
+);
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        headerRight: () => (
-          <Link href="/modal" asChild>
-            <Pressable>
-              {({ pressed, focused }) => (
-                <FontAwesome
-                  name="info-circle"
-                  size={25 * scale}
-                  style={{
-                    marginRight: 15 * scale,
-                    backgroundColor: focused ? '#ddddff' : undefined,
-                    opacity: pressed ? 0.5 : 1,
-                  }}
-                />
-              )}
-            </Pressable>
-          </Link>
-        ),
-        tabBarPosition: 'top',
-        tabBarLabelStyle: {
-          fontSize: 20 * scale,
-          lineHeight: 40 * scale,
-        },
-        tabBarIcon: () => null,
-        tabBarIconStyle: {
-          width: 50 * scale,
-          height: 50 * scale,
-        },
-        tabBarStyle: {
-          height: 100 * scale,
-        },
-        tabBarItemStyle: {
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: -10 * scale,
-        },
-        tabBarActiveBackgroundColor: '#ccccff',
-      }}
-    >
+    <Tabs sidebarAdaptable>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: () =>
+            Platform.OS === 'ios' ? { sfSymbol: 'house' } : null,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
           title: 'Two',
+          tabBarIcon: () =>
+            Platform.OS === 'ios' ? { sfSymbol: 'alt' } : null,
         }}
       />
       <Tabs.Screen
         name="tvdemo"
         options={{
           title: 'TV Demo',
+          tabBarIcon: () => (Platform.OS === 'ios' ? { sfSymbol: 'tv' } : null),
         }}
       />
     </Tabs>
